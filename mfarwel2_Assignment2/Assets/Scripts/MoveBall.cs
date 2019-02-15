8ae40526a5bdaa267d1b5fa7ce23abc1e97054ba
Maddi Farwell <mfarwel2@uwo.ca>
2019-02-14 20:48:07 -0500

    Add Assn2

---
+++ mfarwel2_Assignment2/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveBall : MonoBehaviour
+{
+    public float speed;
+    public Text scoreText;
+
+    private Rigidbody rbody;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        score = 0;
+        scoreText.text = "Score: ";
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveH, 0.0f, moveV);
+
+        rbody.AddForce(move * speed);
+    }
+
+    void OnTriggerEnter(Collider cube)
+    {
+       cube.gameObject.SetActive(false);
+            if (cube.CompareTag("Blue"))
+            {
+                score += 10;
+            }
+            else if (cube.CompareTag("Green"))
+            {
+                score += 20;
+            }
+            if (cube.CompareTag("Yellow"))
+            {
+                score += 30;
+            }
+            SetScore();
+    }
+
+    // Set score function to update score when ball collides. Also tells user when they win
+    void SetScore()
+    {
+        scoreText.text = "Score: " + score.ToString();
+
+        if (score >= 190)
+        {
+            scoreText.text = "You Win!";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ mfarwel2_Assignment2/Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public GameObject prefab;
+    public int numPrefabs = 10;
+    public float radius = 3.0f;
+    public Material material1;
+    public Material material2;
+    public Material material3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int switchCase;
+        for (int i = 0; i < numPrefabs; i++)
+        {
+            float angle = i * Mathf.PI *2 / numPrefabs;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), 0.1f, Mathf.Sin(angle)) * radius;
+            Instantiate(prefab, pos, Quaternion.Euler(45f, 45f, 45f));
+
+            // use mod to instantiate a different ball colour each time
+            switchCase = i % 3;
+            switch (switchCase)
+            {
+                case 0:
+                    prefab.GetComponent<MeshRenderer>().material = material1;
+                    prefab.transform.tag = "Blue";
+                    break;
+                case 1:
+                    prefab.GetComponent<MeshRenderer>().material = material2;
+                    prefab.transform.tag = "Green";
+                    break;
+                case 2:
+                    prefab.GetComponent<MeshRenderer>().material = material3;
+                    prefab.transform.tag = "Yellow";
+                    break;
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
